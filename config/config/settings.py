68c4451754e885cc0df9a2959af91fee392163a8
icis-04 <85933343+icis-04@users.noreply.github.com>
2022-10-15 20:01:52 +0100

    changed database to postgres

--- config/config/settings.py
+++ config/config/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'kJqW1hxoO8dRFRH55yHO',
+        'HOST': 'containers-us-west-100.railway.app',
+        'PORT': '7426',
     }
 }
 
